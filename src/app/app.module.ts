a5034b2e9870f28679c4e90eef5aac5eefa07627
Felipe Larraguibel <felipelarra.araya@gmail.com>
2021-03-07 21:05:24 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PaisModule } from './pais/pais.module';
+import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    PaisModule,
+    SharedModule,
+    
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+  styles: [
+  ]
+})
+export class PorPaisComponent   {
+
+  termino: string = ''; 
+  hayError: boolean = false;
+  paises : Country[] = [];
+  constructor( private paisService: PaisService) { }
+
+  buscar( termino: string){
+    this.hayError = false;
+    this.termino = termino;
+     this.paisService.buscarPais( this.termino )
+    .subscribe( resp =>{
+      console.log(resp);
+      this.paises = resp;
+      
+    }, (err) =>{
+      this.hayError = true;
+      console.log('error')
+      this.paises = [];
+    });
+  }
+
+  sugerencias( termino: string ){
+    this.hayError = false;
+  }
+}
